932e72d0d9686ed734596dc60a73a1dbf7b0c498
jonatan.lara <jonatan.lara@gft.com>
2021-11-20 22:49:04 +0100

    Antes del refactoring

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,13 +1,15 @@
 class ProductList extends React.Component {
-    state = {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       products: [],
     };
 
-  componentDidMount() {
-    this.setState({products: Seed.products });
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
-
-  handleProductUpVote = (productId) => {
+  handleProductUpVote(productId) {
+    console.log(productId + " was upvoted.");
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -17,66 +19,66 @@ class ProductList extends React.Component {
         return product;
       }
     });
-    this.setState({
-      products: nextProducts,
-    });
+
+    this.setState({ products: nextProducts });
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
   }
 
   render() {
-    const products = this.state.products.sort((a, b) => (
-      b.votes - a.votes
-    ));
-    const productComponents = products.map((product) => (
-      <Product
-      key={'product-' + product.id}
-      id={product.id}
-      title={product.title}
-      description={product.description}
-      url={product.url}
-      votes={product.votes}
-      submitterAvatarUrl={product.submitterAvatarUrl}
-      productImageUrl={product.productImageUrl}
-      onVote={this.handleProductUpVote}
-      />
-    ));
-    return (
-      <div className='ui unstackable items'>
-        {productComponents}
-      </div>
-    );
+    const products = this.state.products
+      .sort((a, b) => b.votes - a.votes)
+      .map((product) => (
+        <Product
+          key={"product-" + product.id}
+          id={product.id}
+          onVote={this.handleProductUpVote}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+        />
+      ));
+    return <div className="ui unstackable items">{products}</div>;
   }
 }
 
 class Product extends React.Component {
-  handleUpVote = () => (
-    this.props.onVote(this.props.id)
-  );
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
 
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
+      <div className="item">
+        <div className="image">
           <img src={this.props.productImageUrl} />
         </div>
-        <div className='middle aligned content'>
-          <div className='header'>
+        <div className="middle aligned content">
+          <div className="header">
             <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
+              <i className="large caret up icon" />
             </a>
             {this.props.votes}
           </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
-          <div className='extra'>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
-              className='ui avatar image'
+              className="ui avatar image"
               src={this.props.submitterAvatarUrl}
             />
           </div>
@@ -86,7 +88,4 @@ class Product extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById("content"));
