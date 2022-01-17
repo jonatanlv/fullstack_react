0245456ba49c55b2eb3de51358f46191183e2118
jonatan.lara <jonatan.lara@gft.com>
2022-01-17 19:10:52 +0100

    Cambios acumulados

--- forms/src/06-state-input-multi.js
+++ forms/src/06-state-input-multi.js
@@ -1,69 +1,133 @@
-import React from 'react';
+import React from "react";
 
-const content = document.createElement('div');
+const content = document.createElement("div");
 document.body.appendChild(content);
+
+class Field extends React.Component {
+  handleChange = (evt) => {
+    const value = evt.target.value;
+    let errors;
+    if (this.props.validate) {
+      errors = this.props.validate(value);
+    }
+    this.props.onChangeHandler({ name: this.props.name, value, errors });
+  };
+
+  render() {
+    return (
+      <input
+        placeholder={this.props.placeholder}
+        name={this.props.name}
+        value={this.props.value}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
 
 module.exports = class extends React.Component {
   static displayName = "06-state-input-multi";
 
   state = {
     fields: {
-      name: '',
-      email: ''
+      name: {
+        default: "",
+        value: "",
+        placeholder: "Name",
+      },
+      email: {
+        default: "",
+        value: "",
+        placeholder: "EMail",
+        validate: (mail) => {
+          if (mail.contains("@")) {
+            return null;
+          } else {
+            return "EMail no válido";
+          }
+        },
+      },
     },
+    errors: {},
     people: [],
   };
 
   onFormSubmit = (evt) => {
-    const people = [
-      ...this.state.people,
-      this.state.fields,
-    ];
-    this.setState({ 
-      people, 
-      fields: {
-        name: '',
-        email: ''
-      } 
+    const fs = Object.assign({}, this.state.fields);
+
+    let person = {};
+    for (let p in fs) {
+      if (fs.hasOwnProperty(p)) {
+        person[p] = fs[p].value;
+        fs[p].value = fs[p].default;
+      }
+    }
+
+    const people = [...this.state.people, person];
+    this.setState({
+      people,
+      fields: fs,
+      errors: {},
     });
     evt.preventDefault();
   };
 
-  onInputChange = (evt) => {
-    const fields = this.state.fields;
-    fields[evt.target.name] = evt.target.value;
-    this.setState({ fields });
+  onInputChange = (update) => {
+    const name = update.name;
+
+    const fieldInfo = Object.assign({}, this.state.fields[name]);
+    fieldInfo.value = update.value;
+
+    const updateField = {};
+    updateField[name] = fieldInfo;
+
+    const fields = Object.assign({}, this.state.fields, updateField);
+
+    const errors = Object.assign({}, this.state.errors);
+    if (update.errors) {
+      errors[name] = update.errors;
+    } else {
+      delete errors[name];
+    }
+    this.setState({ errors, fields });
   };
 
   render() {
+    const inputs = [];
+    for (let p in this.state.fields) {
+      if (this.state.fields.hasOwnProperty(p)) {
+        console.log(p);
+        const { placeholder, value } = this.state.fields[p];
+        console.log(value);
+        inputs.push(
+          <Field
+            placeholder={placeholder}
+            key={p}
+            name={p}
+            value={value}
+            onChangeHandler={this.onInputChange}
+          />
+        );
+      }
+    }
     return (
       <div>
         <h1>Sign Up Sheet</h1>
 
         <form onSubmit={this.onFormSubmit}>
-          <input
-            placeholder='Name'
-            name='name'
-            value={this.state.fields.name}
-            onChange={this.onInputChange}
-          />
+          {inputs}
 
-          <input
-            placeholder='Email'
-            name='email'
-            value={this.state.fields.email}
-            onChange={this.onInputChange}
-          />
-
-          <input type='submit' />
+          <input type="submit" />
         </form>
 
         <div>
           <h3>People</h3>
           <ul>
-            { this.state.people.map(({ name, email }, i) =>
-              <li key={i}>{name} ({ email })</li>
-            ) }
+            {this.state.people.map(({ name, email }, i) => (
+              <li key={i}>
+                {name} ({email})
+              </li>
+            ))}
           </ul>
         </div>
       </div>
